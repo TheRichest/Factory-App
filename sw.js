e971a71dd43226a78736c95680db6784c1436d3e
TheRichest <rajprithvi1999@gmail.com>
2026-02-02 19:03:08 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'factoryconnect-v1';
+const CACHE_NAME = 'factoryconnect-v5';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -7,30 +7,20 @@ const urlsToCache = [
   '/icon-512.png'
 ];
 
-// Install service worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch from cache or network
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Return cached version or fetch from network
-        return response || fetch(event.request);
-      }
-    )
+      .then(response => response || fetch(event.request))
   );
 });
 
-// Update service worker
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
